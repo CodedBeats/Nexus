56b00aa957ac8a19aa757d4287d001a1d76bddd0
Luca <luca.haar@icloud.com>
2023-07-03 01:54:58 +1000

    side nav, collapsed nav, open nav -- created a side navbar thats loaded into a page just with js, the default possition of the nav is collapsed being thin with just icons, the nav can be expanded to show more information (there is a nice transition between both)

--- reusable components/navbar/navbarComponent.js
+++ reusable components/navbar/navbarComponent.js
@@ -1,24 +1,89 @@
-// Example reusable component
-let createNavbarComponent = (navTitle1, navTitle2, navTitle3) => {
+// ===== Navbar component ===== //
+let createNavbarComponent = () => {
     let componentHTML = `
-    <div class="navbar">
-    <ul>
-        <li><a href="#">${navTitle1}</a></li>
-        <li><a href="#">${navTitle2}</a></li>
-        <li><a href="#">${navTitle3}</a></li>
-    </ul>
+    <div id="dom-side-nav" class="side-nav">
+        <button onclick="toggleNav()" type="button" id="open-nav-btn">Nav >></button>
+        <div class="nav-menu">
+            <div id="open-nav-menu" style="display:none;">
+                <a href="#">Home</a>
+                <a href="#">Games</a>
+                <a href="#">Link</a>
+            </div>
+            <div id="collapsed-nav-menu">
+                <a href="#">icon</a>
+                <a href="#">icon</a>
+                <a href="#">icon</a>
+            </div>
+        </div>
+        <div class="nav-categories">
+            <div id="open-nav-categories" style="display:none;">
+                <p>Popular Genres</p>
+                <a href="#">Puzzle</a>
+                <a href="#">Stratergy</a>
+                <a href="#">Idle</a>
+            </div>
+            <div id="collapsed-nav-categories">
+                <a href="#">icon</a>
+            </div>
+        </div>
     </div>
     `;
 
     return componentHTML;
 }
 
-// Generate a component and render it
-let navbarComponentData = {
-    navTitle1: "Home",
-    navTitle2: "About",
-    navTitle3: "Games",
-};
 
-const componentHTML = createNavbarComponent(navbarComponentData.navTitle1, navbarComponentData.navTitle2, navbarComponentData.navTitle3);
+// ===== Navbar functionality ===== //
+// Toggle Nav
+let toggleNav = () => {
+    // elements
+    const sideNav = document.getElementById("dom-side-nav")
+    const toggleNavBtn = document.getElementById("open-nav-btn")
+    // store in arr and foreach
+    const openMenu = document.getElementById("open-nav-menu")
+    const collapsedMenu = document.getElementById("collapsed-nav-menu")
+    const openCategories = document.getElementById("open-nav-categories")
+    const collapsedCategories = document.getElementById("collapsed-nav-categories")
+
+    if (openMenu.style.display === "none") {
+        toggleNavBtn.innerHTML = "Nav <<"
+
+        openMenu.style.display = "block"
+        collapsedMenu.style.display = "none"
+        openCategories.style.display = "block"
+        collapsedCategories.style.display = "none"
+        
+        // phone view adjustments 
+        if (window.screen.width <= 800) {
+            console.log(sideNav)
+            setTimeout(() => { sideNav.style.width = "100vw"}, 0);
+            document.body.style.height = "100%"
+            document.body.style.marginLeft = "100%"
+        } 
+        // desktop view adjustments
+        else {
+            setTimeout(() => { sideNav.style.width = "15vw" }, 0);
+            document.body.style.marginLeft = "15vw"
+            document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
+        }
+    } else {
+        toggleNavBtn.innerHTML = "Nav >>"
+
+        openMenu.style.display = "none"
+        collapsedMenu.style.display = "block"
+        openCategories.style.display = "none"
+        collapsedCategories.style.display = "block"
+
+        sideNav.style.width = "7vw";
+        document.body.style.marginLeft = "7vw";
+        document.body.style.backgroundColor = "white";
+        setTimeout(() => { 
+        }, 500);
+    }
+}
+
+
+
+// ===== Load Navbar ===== //
+const componentHTML = createNavbarComponent();
 document.getElementById("navbar-container").innerHTML = componentHTML;
