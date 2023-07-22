791e3b4bc3a11a01d984bdcf2a67d20edbaaebd1
Luca <51949986+CodedBeats@users.noreply.github.com>
2023-07-22 17:24:20 +1000

    Firebase dev2 (#19)
    * del html
    --
    deleted html header file cause it's now js
    
    * linked pages, side nav
    --
    linked all poges together (rather poorly, pls fix),
    added proper navigation to sidebar
    
    * fix
    --
    attempt 2 at fixing website nav
    
    * firebase integration setup
    --
    just commiting this working version now before I break it and can't go back
    
    * firebase testing, CRUD, update
    --
    re-worked the testing page for firebase,
    started making the CRUD files needed for normal stuff (not auth),
    added an update file which currently increases poularity val on a button click (but page needs to be reloaded do to do over and over)
    
    * updateDoc func
    --
    changed the update document function to still be able to =+1 a val but also be more coherent with good js style (might need to pass some vals as params to re-use the func)

--- firebase/update.js
+++ firebase/update.js
@@ -1,18 +1,31 @@
 import { doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js"
 import { db } from "./firebaseConfig.js"
 
-// doc(database, collection, id)
-const gameRef = doc(db, "popularity", "game1");
-let docSnap = await getDoc(gameRef);
+// Function to update a document's value in Firestore
+async function updateDocument() {
+    // change these to params maybe
+    const collectionName = "popularity";
+    const docId = "game1";
+    const fieldName = "pop";
 
+    // Get the current value from Firestore
+    const docRef = doc(db, collectionName, docId);
+    const docSnapshot = await getDoc(docRef);
+    const currentValue = docSnapshot.data()[fieldName];
 
+    // Modify the value
+    const updatedValue = currentValue + 1;
 
-let updatePop = () => {
-    // set value
-    updateDoc(gameRef, {
-        pop: docSnap.data().pop += 1,
-    });
-    console.log(docSnap.data())
+    // Use the updateDoc function to update the specific field
+    const data = { [fieldName]: updatedValue };
+
+    updateDoc(docRef, data)
+        .then(() => {
+            console.log("Document updated successfully!");
+        })
+        .catch((error) => {
+            console.error("Error updating document:", error);
+        });
 }
 
-export { updatePop }
+export { updateDocument }
