4bfab0520230166e98f9b9149dbd2f7e465cb22d
Luca <51949986+CodedBeats@users.noreply.github.com>
2023-07-24 20:04:36 +1000

    Firebase dev5 (#22)
    * del html
    --
    deleted html header file cause it's now js
    
    * linked pages, side nav
    --
    linked all poges together (rather poorly, pls fix),
    added proper navigation to sidebar
    
    * fix
    --
    attempt 2 at fixing website nav
    
    * firebase integration setup
    --
    just commiting this working version now before I break it and can't go back
    
    * firebase testing, CRUD, update
    --
    re-worked the testing page for firebase,
    started making the CRUD files needed for normal stuff (not auth),
    added an update file which currently increases poularity val on a button click (but page needs to be reloaded do to do over and over)
    
    * updateDoc func
    --
    changed the update document function to still be able to =+1 a val but also be more coherent with good js style (might need to pass some vals as params to re-use the func)
    
    * create doc, custom id
    --
    added a function (and file) to create documents (might need to add some params),
    added a getTotoalDocs func to set a custom ID for documents (but should be refactored a tad)
    
    * fire dir restructure, read doc func, create doc func refactor
    --
    changed up the file directory folders and names to be more expandable and help me plan for the future,
    added the read doc function to simply read data from a document (no implimentation yet in terms of passing data to html elements),
    refactoreed the create doc function to take params of collection and data to be more usable (but to create a user my be a totally completely different set of functions)
    
    * delete doc
    --
    added a delete doc function with correct collect and id params

--- firebase/CRUD/create.js
+++ firebase/CRUD/create.js
@@ -1,24 +1,21 @@
-import { collection, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js"
-import { db } from "../firebaseConfig.js"
-import { getTotalDocuments } from "../misc.js"
-
+import { collection, doc, setDoc, } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { db } from "../firebaseConfig.js";
+import { getTotalDocuments } from "../misc.js";
 
 async function createDocument(collectionName, newDocumentData) {
-    
     const collectionRef = collection(db, collectionName);
-  
+
     // set custom ID here...maybe turn into a func
-    const documentId = `doc${await getTotalDocuments() + 1}`;
-  
+    const documentId = `doc${(await getTotalDocuments()) + 1}`;
+
     try {
-      // add doc
-      await setDoc(doc(collectionRef, documentId), newDocumentData);
-  
-      console.log("Document created with ID:", documentId);
+        // add doc
+        await setDoc(doc(collectionRef, documentId), newDocumentData);
 
+        console.log("Document created with ID:", documentId);
     } catch (error) {
-      console.error("Error creating document:", error);
+        console.error("Error creating document:", error);
     }
-  }
+}
 
-export { createDocument }
+export { createDocument };
