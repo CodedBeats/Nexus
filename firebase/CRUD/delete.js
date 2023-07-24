2dc339a0298dd513b76cc71ec1a1cb79bcebc93c
Luca <luca.haar@icloud.com>
2023-07-24 19:59:45 +1000

    delete doc -- added a delete doc function with correct collect and id params

---
+++ firebase/CRUD/delete.js
@@ -0,0 +1,17 @@
+// deleteDocument.js
+import { doc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { db } from "../firebaseConfig.js";
+
+async function deleteDocument(collectionName, docID) {
+    const docRef = doc(db, collectionName, docID);
+
+    try {
+        await deleteDoc(docRef);
+        console.log("Document deleted successfully.");
+
+    } catch (error) {
+        console.error("Error deleting document:", error);
+    }
+}
+
+export { deleteDocument };
--- firebase/tests/load.js
+++ firebase/tests/load.js
@@ -1,23 +1,31 @@
-import { getPop, db } from "../firebaseConfig.js"
-import { updateDocument } from "../CRUD/update.js"
-import { createDocument } from "../CRUD/create.js"
-import { getData } from "../CRUD/read.js"
+import { updateDocument } from "../CRUD/update.js";
+import { createDocument } from "../CRUD/create.js";
+import { getData } from "../CRUD/read.js";
+import { deleteDocument } from "../CRUD/delete.js";
 
-
-await getData("popularity", "doc33")
-
-document.getElementById('btn1').addEventListener('click', updateDocument)
+// await getData("popularity", "doc33")
 
 // create doc
-document.getElementById('btn2').addEventListener("click", () => {
-  
+document.getElementById("btn1").addEventListener("click", () => {
     const collectionName = "popularity";
-    
+
     const data = {
         testField: "test field info33 fffffffffffffffffff -- rrgggggggggg",
         // add more
-    }
-  
+    };
+
     createDocument(collectionName, data);
 });
 
+// await getData("popularity", "doc33")
+
+// update doc
+document.getElementById("btn3").addEventListener("click", updateDocument);
+
+// delete doc
+document.getElementById("btn4").addEventListener("click", () => {
+    const collectionName = "popularity";
+    const docId = "doc34";
+
+    deleteDocument(collectionName, docId);
+});
