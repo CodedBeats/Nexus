3a50cc4368c53b5e7136a652e2af8861f0415ec0
Luca <luca.haar@icloud.com>
2023-08-01 18:48:49 +1000

    load user data -- finished the load data function to the point where it works but needs so much adjusting in both html pages and js DOM shtuff

--- firebase/user_auth/loadProfile.js
+++ firebase/user_auth/loadProfile.js
@@ -1,26 +1,26 @@
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
 import { getDoc, doc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
-import { db } from "../misc/firebaseConfig.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+import { db, auth } from "../misc/firebaseConfig.js";
 
 
 async function loadProfile() {
     // Check if a user is logged in
-    const user = getAuth().currentUser;
+    const user = auth.currentUser;
 
     if (user) {
         try {
             // Retrieve the user's profile document from db
             const userRef = doc(db, "users", user.uid);
             const profileSnapshot = await getDoc(userRef);
-            console.log(profileSnapshot);
 
             if (profileSnapshot.exists()) {
                 // Get the user's profile data
                 const profileData = profileSnapshot.data();
+                console.log(profileData.accountName, profileData.email);
 
                 // Update the profile page HTML elements with the user's information
-                document.getElementById("user-display-name").textContent = profileData.username;
-                document.getElementById("user-email").textContent = profileData.email;
+                document.getElementById("field1").textContent = profileData.accountName;
+                document.getElementById("field2").textContent = profileData.email;
                 // Add more fields as needed
 
                 // Show the profile page ig the original profile display = "none"
@@ -38,5 +38,19 @@ async function loadProfile() {
     }
 }
 
-// Call the function to load the profile when the page loads
-document.addEventListener("DOMContentLoaded", loadProfile);
+
+// Listen for changes in authentication state
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log("User is signed in:", user);
+
+        // Call the function to load the profile when the page loads
+        loadProfile()
+
+    } else {
+        // User is signed out
+        console.log("User is signed out.");
+        
+        // You can redirect the user to the login page or perform other actions as needed.
+    }
+});
