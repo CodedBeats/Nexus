530482a6a27855debf03265c4be647d0cc172825
Luca <luca.haar@icloud.com>
2023-08-02 15:15:41 +1000

    toggle nav, icon names, header content -- got rid of toggle nav functionality cause it's just kinda unnecessary and won't be used, added names to the icons on the side nav, changed the header content getting rid of menu button and adjusting styles

--- reusable_components/header/headerComponent.js
+++ reusable_components/header/headerComponent.js
@@ -2,27 +2,20 @@
 let createHeaderComponent = () => {
     let headerComponentHTML = `
     <div class="header">
-        <div class="left-container">
-            <div id="navbarToggleIcon" class="navbarIcon"><button onclick="toggleNav()" type="button"><i class="fa fa-bars fa-3x"></i></button></div>
+        <div class="search-container">
+            <form class="search-form">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    class="search-input"
+                />
+                <button type="submit">
+                    <i class="fa fa-search fa-lg"></i>
+                </button>
+            </form>
         </div>
-        <div class="middle-container">
-            <div class="search-container">
-                <form class="search-form">
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        class="search-input"
-                    />
-                    <button type="submit">
-                        <i class="fa fa-search fa-lg"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
-        <div class="right-container">
-            <div class="profile">
-                <a href="../user_page/userpage-profile.html"><i class="fa fa-user-circle-o fa-3x"></i></a>
-            </div>
+        <div class="profile">
+            <a href="../user_page/userpage-profile.html"><i class="fa fa-user-circle-o fa-3x"></i></a>
         </div>
     </div>
     `;
