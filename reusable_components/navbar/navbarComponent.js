f7014133bbf08b86450c78c61ed7c9f0876aa839
Luca <51949986+CodedBeats@users.noreply.github.com>
2023-08-02 15:16:42 +1000

    Nav ux (#47)
    * del html
    --
    deleted html header file cause it's now js
    
    * linked pages, side nav
    --
    linked all poges together (rather poorly, pls fix),
    added proper navigation to sidebar
    
    * fix
    --
    attempt 2 at fixing website nav
    
    * firebase integration setup
    --
    just commiting this working version now before I break it and can't go back
    
    * firebase testing, CRUD, update
    --
    re-worked the testing page for firebase,
    started making the CRUD files needed for normal stuff (not auth),
    added an update file which currently increases poularity val on a button click (but page needs to be reloaded do to do over and over)
    
    * updateDoc func
    --
    changed the update document function to still be able to =+1 a val but also be more coherent with good js style (might need to pass some vals as params to re-use the func)
    
    * create doc, custom id
    --
    added a function (and file) to create documents (might need to add some params),
    added a getTotoalDocs func to set a custom ID for documents (but should be refactored a tad)
    
    * fire dir restructure, read doc func, create doc func refactor
    --
    changed up the file directory folders and names to be more expandable and help me plan for the future,
    added the read doc function to simply read data from a document (no implimentation yet in terms of passing data to html elements),
    refactoreed the create doc function to take params of collection and data to be more usable (but to create a user my be a totally completely different set of functions)
    
    * delete doc
    --
    added a delete doc function with correct collect and id params
    
    * firebase folder reorder, fixed styles, firebase test page
    --
    re-ordered the files in the firebase folder to not break the side-nav dir,
    changed styles on the tests page to not overlap with header,
    moved the tests page for firebase to the side-nav and deleted the html component test page cause it's not needed
    
    * folder rename, text dec
    --
    changed folder names to not have spaces,
    uddated the text decoration on about page links
    
    * fixed page linking
    
    * signup, user auth
    --
    created the functionality for users to signup with a credible email and a password longer than 6 characters + a user doc gets created when a user signs up,
    setup user auth in firebase
    
    * firebase file dir
    --
    moved some files into a misc folder and update import statements
    
    * login, consistent user, unfinished
    --
    setup the login functionality with a simple page,
    changed config to make sure user stays logged in when changing pages (and maybe closes and re-opens the window),
    connecting it all together is unfinished and requires a bit of problem solving to properly intergrate
    
    * load user data
    --
    finished the load data function to the point where it works but needs so much adjusting in both html pages and js DOM shtuff
    
    * back-end cleanup
    --
    simply cleaning up some back-end file stuff
    
    * tests page
    --
    removed tests page cause it's no longer needed and shouldn't be displayed on the final product
    
    * cleanup
    --
    cleaned up some code
    
    * fa icons
    --
    added icons to the collapsed side nav
    
    * side nav icons
    --
    added the import for font awesome across every page
    
    * page folders
    --
    moved home page and about page into a folder cause they just sat in the root dir
    
    * img links
    --
    fixed the ref links for images in the about page
    
    * toggle nav, icon names, header content
    --
    got rid of toggle nav functionality cause it's just kinda unnecessary and won't be used,
    added names to the icons on the side nav,
    changed the header content getting rid of menu button and adjusting styles

--- reusable_components/navbar/navbarComponent.js
+++ reusable_components/navbar/navbarComponent.js
@@ -3,24 +3,16 @@ let createNavbarComponent = () => {
     let navbarComponentHTML = `
     <div id="dom-side-nav" class="side-nav">
         <div class="nav-menu">
-            <div id="open-nav-menu" style="display:none;">
-                <a href="../home_page/homepage.html">Home</a>
-                <a href="../about_page/aboutpage.html">About</a>
-            </div>
             <div id="collapsed-nav-menu">
-                <a href="../home_page/homepage.html"><i class="fa-solid fa-house" style="color: #ffffff;"></i></a>
-                <a href="../about_page/aboutpage.html"><i class="fa-solid fa-info" style="color: #ffffff;"></i></a>
+                <a href="../home_page/homepage.html"><i class="fa-solid fa-house" style="color: #ffffff;"></i><br>Home</a>
+                <a href="../about_page/aboutpage.html"><i class="fa-solid fa-info" style="color: #ffffff;"></i><br>About</a>
             </div>
         </div>
         <p id="nav-categories-title">Popular Genres</p>
         <div class="nav-categories">
-            <div id="open-nav-categories" style="display:none;">
-                <a href="#">Puzzle</a>
-                <a href="#">Stratergy</a>
-            </div>
             <div id="collapsed-nav-categories">
-                <a href="#"><i class="fa-solid fa-puzzle-piece fa-fade" style="color: #ffffff;"></i></a>
-                <a href="#"><i class="fa-solid fa-dice-five fa-spin" style="color: #ffffff;"></i></i></a>
+                <a href="#"><i class="fa-solid fa-puzzle-piece" style="color: #ffffff;"></i><br>Puzzle</a>
+                <a href="#"><i class="fa-solid fa-dice-five" style="color: #ffffff;"></i><br>Stratergy</a>
             </div>
         </div>
     </div>
@@ -28,57 +20,6 @@ let createNavbarComponent = () => {
 
     return navbarComponentHTML;
 }
-
-
-// ===== Navbar functionality ===== //
-// Toggle Nav
-let toggleNav = () => {
-    // elements
-    const sideNav = document.getElementById("dom-side-nav")
-    const toggleNavBtn = document.getElementById("navbarToggleIcon")
-    const categoriesTitle = document.getElementById("nav-categories-title")
-    // store in arr and foreach
-    const openMenu = document.getElementById("open-nav-menu")
-    const collapsedMenu = document.getElementById("collapsed-nav-menu")
-    const openCategories = document.getElementById("open-nav-categories")
-    const collapsedCategories = document.getElementById("collapsed-nav-categories")
-
-    if (openMenu.style.display === "none") {
-        // expanded
-        openMenu.style.display = "block"
-        collapsedMenu.style.display = "none"
-        openCategories.style.display = "block"
-        collapsedCategories.style.display = "none"
-        categoriesTitle.style.display = "block"
-        
-        // phone view adjustments 
-        if (window.screen.width <= 800) {
-            // console.log(sideNav)
-            sideNav.style.width = "100vw"
-            document.body.style.height = "100%"
-            document.body.style.marginLeft = "100%"
-        } 
-        // desktop view adjustments
-        else {
-            sideNav.style.width = "15vw" 
-            document.body.style.marginLeft = "15vw"
-            document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
-        }
-    } else {
-        // collapsed
-        openMenu.style.display = "none"
-        collapsedMenu.style.display = "block"
-        openCategories.style.display = "none"
-        collapsedCategories.style.display = "block"
-        categoriesTitle.style.display = "none"
-
-        sideNav.style.width = "7vw";
-        document.body.style.marginLeft = "7vw";
-        document.body.style.backgroundColor = "white";
-    }
-}
-
-
 
 // ===== Load Navbar ===== //
 const navbarComponentHTML = createNavbarComponent();
