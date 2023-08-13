6a56a753ea996a4973f866c2bbb14487b79939e3
Luca <luca.haar@icloud.com>
2023-08-13 22:20:05 +1000

    wip -- added a work in progress overlay to display when there isn't a game to load from a game link clicked

--- game_page/loadGame.js
+++ game_page/loadGame.js
@@ -8,11 +8,21 @@ const scriptElement = document.createElement("script");
 // Set the source of the script based on the selected game
 if (gameId === "snake") {
     loadGame("../games/snake/snake.js", "../games/snake/snake.css", 900, 600);
-} else if (gameId === "pong") {
-    loadGame("../games/pong/pong.js", "../games/pong/pong.css");
+} 
+else if (gameId === "pong") {
+    console.log("pong game")
 }
 // Add more conditions for other games if needed
 
+// game not available yet
+else {
+    const canvas = document.getElementById('gameCanvas');
+    console.log("WIP");
+    loadWip();
+    canvas.style.display = "none";
+}
+
+// load apropriate game and set canvas width and height
 function loadGame(scriptSrc, styleHref, canvasWidth, canvasHeight) {
     const scriptElement = document.createElement("script");
     scriptElement.src = scriptSrc;
@@ -24,6 +34,12 @@ function loadGame(scriptSrc, styleHref, canvasWidth, canvasHeight) {
     const canvas = document.getElementById('gameCanvas');
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
+}
+
+// load WIP overlay
+function loadWip() {
+    const wipOverlay = document.getElementById('wip');
+    wipOverlay.classList.add('active');
 }
 
 // Append the script element to the page
