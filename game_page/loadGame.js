c595dcd28d52ed29663b7c4baa2c2079e3a785c0
Luca <luca.haar@icloud.com>
2023-08-15 17:42:42 +1000

    page title -- added a page title (thing that shows up on the tab) to every page including dynamically loading the current game name to the game page

--- game_page/loadGame.js
+++ game_page/loadGame.js
@@ -5,6 +5,11 @@ const gameId = urlParams.get("gameId");
 // Create a script element
 const scriptElement = document.createElement("script");
 
+// set page title to game name
+const pageTitle = document.getElementById("page-title");
+const gameName = gameId.charAt(0).toUpperCase() + gameId.slice(1);
+pageTitle.textContent = `${gameName} - Nexus`;
+
 // Set the source of the script based on the selected game
 if (gameId === "snake") {
     console.log("Snake game")
