8510793d6593313bf36c3fddb33eaae803e23cc3
Luca <luca.haar@icloud.com>
2023-08-20 18:00:33 +1000

    delete acc fix -- fixed delete account functionality (turns out it just had to do with routing to a different page at the wrong time)

--- user_page/deleteAccount.js
+++ user_page/deleteAccount.js
@@ -22,69 +22,65 @@ function deleteAccount() {
 
         // Try to re-authenticate the user first
         reauthenticateWithCredential(currentUser, credential)
-            .then(() => {
-                // User successfully re-authenticated, proceed with account deletion
-                if (
-                    confirm(
-                        "Are you sure you want to delete your account? This action cannot be undone."
-                    )
-                ) {
-                    // Delete the user account first
-                    currentUser
-                        .delete()
-                        .then(() => {
-                            // User account deletion successful, now delete the Firestore document associated with the user
-                            deleteDoc(userDocRef)
-                                .then(() => {
-                                    // Document deletion successful, handle any further actions (e.g., sign out)
-                                    alert(
-                                        "Account and associated document deleted successfully."
-                                    );
-                                })
-                                .catch((error) => {
-                                    // Document deletion failed, handle the error
-                                    alert(
-                                        "An error occurred while deleting the associated document. Please try again later."
-                                    );
-                                    console.error(error);
-                                });
-                        })
-                        .catch((error) => {
-                            // User account deletion failed, handle the error
-                            alert(
-                                "An error occurred while deleting the account. Please try again later."
-                            );
-                            console.error(error);
-                        });
-                        
-                    // link to home
-                    window.location.href = "../home_page/homepage.html";
-                }
-            })
-            .catch((error) => {
-                // Re-authentication failed, handle the error
-                console.error(error);
-            });
-
-    } else {
-        // User is not logged in, handle accordingly (e.g., show a login prompt)
+        .then(() => {
+            // User successfully re-authenticated, proceed with account deletion
+            if (
+                confirm(
+                    "Are you sure you want to delete your account? This action cannot be undone."
+                )
+            ) {
+                // Delete the user account first
+                currentUser
+                    .delete()
+                    .then(() => {
+                        // User account deletion successful, now delete the Firestore document associated with the user
+                        deleteDoc(userDocRef)
+                            .then(() => {
+                                // Document deletion successful, handle any further actions (e.g., sign out)
+                                alert(
+                                    "Account deleted successfully."
+                                );
+                    
+                                // link to home
+                                setTimeout(window.location.href = "../home_page/homepage.html", 1000);
+                            })
+                            .catch((error) => {
+                                // Document deletion failed, handle the error
+                                alert(
+                                    "An error occurred while deleting the associated document. Please try again later."
+                                );
+                                console.error(error);
+                            });
+                    })
+                    .catch((error) => {
+                        // User account deletion failed, handle the error
+                        alert(
+                            "An error occurred while deleting the account. Please try again later."
+                        );
+                        console.error(error);
+                    });
+            }
+        })
+        .catch((error) => {
+            // Re-authentication failed, handle the error
+            console.error(error);
+        });
+    }
+    else {
         alert("You must be logged in to delete your account.");
     }
 }
 
 // Add event listener to the delete button
 const deleteButton = document.getElementById("delete-acc-btn");
+deleteButton.addEventListener("click", deleteAccount);
 
 // Listen for changes in authentication state
 onAuthStateChanged(auth, (user) => {
     if (user) {
-        // diaply loading page
-        deleteButton.addEventListener("click", deleteAccount);
-
+        // user is signed in
     } else {
         // User is signed out
         console.log("User is signed out.");
-
-        // You can redirect the user to the login page or perform other actions as needed.
     }
 });
